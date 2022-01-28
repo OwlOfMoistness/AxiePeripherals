580df0a31569b306dcbfbbec73d6c8033e6f7058
OwlOfMoistness <owl@yieldguild.games>
2022-01-28 10:44:00 +0100

    First

---
+++ contracts/erc1155Dispatcher.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+
+contract ERC1155Dispatcher {
+
+	function disperseOneToken(IERC1155 _token, uint256 _id, address[] calldata _recipients, uint256[] calldata _values) public {
+		require(_recipients.length == _values.length, "ERC1155Dispatcher: Arrays not same length");
+		for (uint256 i = 0; i < _values.length; i++)
+			_token.safeTransferFrom(msg.sender, _recipients[i], _id, _values[i], "");
+	}
+
+	function disperseMany(IERC1155 _token, uint256[] calldata _ids, address[] calldata _recipients, uint256[] calldata _values) public {
+		require(_recipients.length == _values.length && _recipients.length == _values.length, "ERC1155Dispatcher: Arrays not same length");
+		for (uint256 i = 0; i < _values.length; i++)
+			_token.safeTransferFrom(msg.sender, _recipients[i], _ids[i], _values[i], "");
+	}
+
+	function disperseBatchToken(IERC1155 _token, uint256[] calldata _ids, address[] calldata _recipients, uint256[][] calldata _values) public {
+		require(_recipients.length == _values.length, "ERC1155Dispatcher: Arrays not same length");
+		for (uint256 i = 0; i < _values.length; i++)
+			_token.safeBatchTransferFrom(msg.sender, _recipients[i], _ids, _values[i], "");
+	}
+}
